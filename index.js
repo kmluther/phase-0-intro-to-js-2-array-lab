641a6f2f4d33741643c4a3eca13f508cc4b893f1
Katelyn Luther <katelynluther87@aim.com>
2023-01-17 15:35:24 -0700

    commit tree 47a1c117eceb3c00fa9c20c6125815c539c04214

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+function appendCat(name) {
+   const newCats = [...cats, (name)];
+   return newCats;
+}
+function prependCat(name) {
+    const newCats2 = [(name), ...cats];
+    return newCats2;
+}
+function removeLastCat() {
+    const newCats3 = [...cats.slice(0, -1)];
+    return newCats3;
+}
+function removeFirstCat() {
+    const newCats4 = [...cats.slice(1)];
+    return newCats4;
+}
